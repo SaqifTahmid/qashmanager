8a7494d576067800c781487f3b418a39950851cd
SaqifTahmid <161623686+SaqifTahmid@users.noreply.github.com>
2024-06-14 01:34:23 -0400

    Add files via upload

---
+++ env/config.py
@@ -0,0 +1,6 @@
+#token data
+IEX_API_TOKEN= 'pk_a4e62660a9c642a2aaa26511076c524b'
+
+#db setup
+SQLALCHEMY_DATABASE_URI='sqlite:///C:/Users/Spring Boots/Desktop/App/tradingplatform/db/database.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
